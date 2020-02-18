4b80139e497d017b45f94d04bd2ce12a80b8a4f1
James Lenahan <james.k.lenahan@gmail.com>
2020-02-18 16:24:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -17,24 +18,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    # binding.pry
-    @user = User.new(:username => params[:username], :password => params[:password], :balance => 0, :acct_num => rand(10**10))
-    
-    if @user.username == "" || @user.password == ""
-      redirect "/failure"
-    elsif @user.save
-      redirect "/login"
-    else 
-      redirect "/failure"
+    if params[:username].empty?
+      redirect to '/failure'
+    end
+
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect '/login'
+    else
+      redirect '/failure'
     end
   end
 
   get '/account' do
-    if logged_in?
-      erb :account
-    else 
-      redirect '/failure'
-    end
+    @user = User.find(session[:user_id])
+    erb :account
   end
 
 
@@ -43,16 +41,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    session.clear
-    @user = User.find_by(:username => params[:username])
-    
-    if params[:username] == "" || params[:password] == ""
-      redirect '/failure'
-    elsif @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/account'
-    else 
-      redirect '/failure'
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
+  end
+
+  get "/success" do
+    if logged_in?
+      erb :success
+    else
+      redirect "/login"
     end
   end
 
@@ -63,27 +66,6 @@ class ApplicationController < Sinatra::Base
   get "/logout" do
     session.clear
     redirect "/"
-  end
-
-  get "/badwithdraw" do
-    erb :badwithdraw
-  end
-
-  post "/deposit" do
-    binding.pry
-    current_user.update(balance: (current_user.balance + params[:deposit].to_i))
-    
-    redirect '/account'
-  end
-
-  post "/withdraw" do
-    binding.pry
-    if current_user.balance - params[:withdraw].to_i >= 0
-      current_user.update(balance: (current_user.balance - params[:withdraw].to_i))
-      redirect '/account'
-    else 
-      redirect '/lure'
-    end
   end
 
   helpers do
